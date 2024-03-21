b08d4ef5bd7cb6feda78398644202dadd12a6304
Mina <mina9301@gmail.com>
2024-03-21 20:53:13 +0900

    fix: 시침과 분침 스타일 일부 수정 - 시침과 분침의 넓이 5px -> 6px 수정

--- src/constants/clock.ts
+++ src/constants/clock.ts
@@ -4,7 +4,7 @@ export const CLOCK_NUMBERS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
 // 시곗바늘
 export const CLOCK_HAND_STYLE = {
-  hour: { width: 5, height: 60, color: '#ff7306' },
-  minute: { width: 5, height: 80, color: '#ff7306' },
+  hour: { width: 6, height: 60, color: '#ff7306' },
+  minute: { width: 6, height: 80, color: '#ff7306' },
   second: { width: 2, height: 80, color: '#8c8c8b' },
 };
