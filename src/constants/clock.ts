c94f90b23f92a4188a93b8af35e328bf6064c606
Mina <mina9301@gmail.com>
2024-03-23 22:17:30 +0900

    feat: 시곗바늘 세 가지 타입을 리스트 형식으로 생성

--- src/constants/clock.ts
+++ src/constants/clock.ts
@@ -1,8 +1,15 @@
+import { ClockHandType } from './types';
+
 // 시계판
 export const CLOCK_RADIUS = 100;
 export const CLOCK_NUMBERS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
 // 시곗바늘
+export const CLOCK_HAND_TYPE_LIST: { id: string; type: ClockHandType }[] = [
+  { id: 'clock_hand_hour', type: 'hour' },
+  { id: 'clock_hand_minute', type: 'minute' },
+  { id: 'clock_hand_second', type: 'second' },
+];
 export const CLOCK_HAND_STYLE = {
   hour: { width: 6, height: 60, color: '#ff7306' },
   minute: { width: 6, height: 80, color: '#ff7306' },
