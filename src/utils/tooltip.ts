3ec78e52f13b488eba8f342407911e2ae4d1c919
Mina <mina9301@gmail.com>
2024-03-24 12:11:03 +0900

    fix: 오전과 오후 12시일 때 tooltip 시간 값 계산 수정

--- src/utils/tooltip.ts
+++ src/utils/tooltip.ts
@@ -1,6 +1,7 @@
 export function getFormatCurrentTime(time: Date) {
   const amPm = time.getHours() > 11 ? '오후 ' : '오전 ';
-  const currentHour = (time.getHours() % 12) + '시 ';
+  // 12시인 경우에는 값이 0이 되므로 오전 및 오후 모두 12시로 표기되도록 한다.
+  const currentHour = (time.getHours() % 12 || 12) + '시 ';
   const currentMinute = time.getMinutes() + '분';
 
   return amPm.concat(currentHour, currentMinute);
